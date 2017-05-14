9841964e955f79843518389ac3753be14755880c
Gustavo Roríguez <gustavrod@gmail.com>
2017-05-14 13:37:13 +0200

    Enabling CORS

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
 from flask import Flask
 import json
 from service import execute_search, combine_duplicate_businesses, write_businesses
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/api/<distance>/<location>/<query>')
